f2bc1aabb65fa8e111c8938bce06823f3369cd73
Ubuntu <ubuntu@ip-172-31-11-150.ec2.internal>
2020-04-26 20:07:07 -0500

    modify run.py

--- run.py
+++ run.py
@@ -8,9 +8,11 @@
 ##
 __author__ = 'Vas Vasiliadis <vas@uchicago.edu>'
 
+import os
 import sys
 import time
 import driver
+import boto3
 
 """A rudimentary timer for coarse-grained profiling
 """
@@ -29,11 +31,19 @@ class Timer(object):
       print(f"Approximate runtime: {self.secs:.2f} seconds")
 
 if __name__ == '__main__':
-	# Call the AnnTools pipeline
-	if len(sys.argv) > 1:
-		with Timer():
-			driver.run(sys.argv[1], 'vcf')
-	else:
-		print("A valid .vcf file must be provided as input to this program.")
+        # Call the AnnTools pipeline
+        if len(sys.argv) > 1:
+                with Timer():
+                        driver.run(sys.argv[1], 'vcf')
+                filename = sys.argv[1].split('/')[-1]
+                s3 = boto3.client('s3')
+                outputFilename = 'data/' + filename.split('.')[0] + '.annot.vcf'
+                s3.upload_file(Bucket = 'gas-results', Filename = '/home/ubuntu/anntools/' + outputFilename, Key = 'angelo23/' + outputFilename.split('/')[1])
+                logFilename = 'data/' + filename + '.count.log'
+                s3.upload_file(Bucket = 'gas-results', Filename = '/home/ubuntu/anntools/' + logFilename, Key = 'angelo23/' + logFilename.split('/')[1])
+                os.system('rm ' + outputFilename)
+                os.system('rm ' + logFilename)
+        else:
+                print("A valid .vcf file must be provided as input to this program.")
 
 ### EOF
